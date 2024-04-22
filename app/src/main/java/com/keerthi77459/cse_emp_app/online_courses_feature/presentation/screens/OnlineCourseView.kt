2af5354b4fb9cb2bbc7c31d0f8a72a22466e3e64
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-04-22 18:16:04 +0530

    Crafted OnlineCourseScreen Logic

--- app/src/main/java/com/keerthi77459/cse_emp_app/online_courses_feature/presentation/screens/OnlineCourseView.kt
+++ app/src/main/java/com/keerthi77459/cse_emp_app/online_courses_feature/presentation/screens/OnlineCourseView.kt
@@ -2,13 +2,13 @@ package com.keerthi77459.cse_emp_app.online_courses_feature.presentation.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,22 +18,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.keerthi77459.cse_emp_app.R
 import com.keerthi77459.cse_emp_app.core.components.BuildAppBar
 import com.keerthi77459.cse_emp_app.core.components.BuildFloatingActionButton
 import com.keerthi77459.cse_emp_app.core.navigation.NavigationScreen
 import com.keerthi77459.cse_emp_app.online_courses_feature.data.OnlineCoursesData
 import com.keerthi77459.cse_emp_app.online_courses_feature.domain.services.FetchOnlineCourseDetails
+import com.keerthi77459.cse_emp_app.online_courses_feature.presentation.components.BuildListView
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun OnlineCoursesView(navController: NavController) {
 
-    var onlineCourseDetails by remember { mutableStateOf<List<OnlineCoursesData>>(listOf())  }
+    var onlineCourseDetails by remember { mutableStateOf<List<OnlineCoursesData>>(listOf()) }
 
     LaunchedEffect(Unit) {
         FetchOnlineCourseDetails().fetchDetails(
-            onSuccess = {data ->
+            onSuccess = { data ->
                 onlineCourseDetails = data
             }, onFailure = {
                 println(it.message)
@@ -44,19 +44,27 @@ fun OnlineCoursesView(navController: NavController) {
     println(onlineCourseDetails)
 
     Scaffold(
-        topBar = { BuildAppBar(title = "Online Courses", icon = R.drawable.baseline_logout_24) {} },
+        topBar = {
+            BuildAppBar(
+                title = "Online Courses",
+                showIcon = false,
+                icon = Icons.Filled.Add
+            ) {}
+        },
         floatingActionButton = {
             BuildFloatingActionButton(icon = Icons.Filled.Add) {
                 navController.navigate(NavigationScreen.OnlineCoursesScreen.route)
             }
         }
     ) {
-        LazyColumn (
+        Column(
             modifier = Modifier.padding(it),
             verticalArrangement = Arrangement.spacedBy(16.dp),
-        ){
-            itemsIndexed(onlineCourseDetails) {_,data ->
-                Text(text = data.course)
+        ) {
+            LazyColumn {
+                itemsIndexed(onlineCourseDetails) { _, data ->
+                    BuildListView(onlineCoursesData = data)
+                }
             }
         }
     }
