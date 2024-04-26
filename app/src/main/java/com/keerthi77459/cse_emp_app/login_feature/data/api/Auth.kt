931122a3547a2e02023a7aea9a9d6d390516dd6d
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-04-27 00:10:35 +0530

    Changed Login Logic

--- app/src/main/java/com/keerthi77459/cse_emp_app/login_feature/data/api/Auth.kt
+++ app/src/main/java/com/keerthi77459/cse_emp_app/login_feature/data/api/Auth.kt
@@ -1,11 +1,16 @@
 package com.keerthi77459.cse_emp_app.login_feature.data.api
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+
 
 class Auth {
     var auth: FirebaseAuth = FirebaseAuth.getInstance()
     var loginState = MutableLiveData<LoginState>()
+    var resetState = MutableLiveData<ResetState>()
     fun userLogin(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -16,9 +21,25 @@ class Auth {
                 }
             }
     }
+
+    fun resetPassword(userName: String) {
+        auth.sendPasswordResetEmail(userName)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    resetState.postValue(ResetState.Success)
+                } else {
+                    resetState.postValue(ResetState.Error("Invalid Credentials"))
+                }
+            }
+    }
 }
 
 sealed class LoginState {
     data object Success : LoginState()
     data class Error(val message: String) : LoginState()
+}
+
+sealed class ResetState {
+    data object Success : ResetState()
+    data class Error(val message: String) : ResetState()
 }
